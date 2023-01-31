35d3da2d667256412d4d21dc8802f74160ba405e
shravani Patnamshetty <spatnams@spatnams.remote.csb>
2023-01-31 17:02:42 +0530

    made final changes

---
+++ oddeven.java
@@ -0,0 +1,13 @@
+public class oddeven{
+
+    public static void main(String[] args){
+        int a=5;
+
+        if(a%2==0){
+            System.out.println("a is even");
+        }
+        else{
+          System.out.println("a is odd");  
+        }
+    }git
+}
